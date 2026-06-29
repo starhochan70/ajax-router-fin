9bd0b94e6d2a95681c31fa5c7850a10b2ba4fc7a
최호찬 <starhochan70@gmail.com>
2026-06-29 17:07:34 +0900

    Add gh-pages dependency and update deployment script; modify fetch URL in App component; switch from BrowserRouter to HashRouter; fix formatting in vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
     async function fetchData() {
       try {
-        const res = await fetch("/data/blog.json", {
+        const res = await fetch(`${import.meta.env.BASE_URL}data/blog.json`, {
           signal: controller.signal,
         });
         if (!res.ok) throw new Error("메시지");
