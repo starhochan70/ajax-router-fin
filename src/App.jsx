3f29fa7f103f39ac8818983822bcd5caa40570ff
최호찬 <starhochan70@gmail.com>
2026-06-29 16:14:24 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect, useMemo } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
+import NotFound from "./pages/NotFound";
+import PostNew from "./pages/PostNew";
+import PostEdit from "./pages/PostEdit";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // let alive = true; //상품조회 시작..열일중...
+    const controller = new AbortController();
+
+    async function fetchData() {
+      try {
+        const res = await fetch("/data/blog.json", {
+          signal: controller.signal,
+        });
+        if (!res.ok) throw new Error("메시지");
+        const data = await res.json();
+        setPosts(data);
+      } catch (e) {
+        console.error(e);
+        setPosts([]); //에러시 목록 비움
+      } finally {
+        setLoaded(true);
+      }
+    }
+    fetchData();
+
+    return () => {
+      // alive = false;
+      controller.abort();
+    }; //정리함수
+  }, []);
+
+  const onDelete = _id => {
+    setPosts(prev => prev.filter(post => post.id !== _id));
+  };
+  const newId = useMemo(() => {
+    const maxId = posts.reduce((acc, current) => {
+      return Math.max(acc, current.id);
+    }, 0);
+    return maxId + 1;
+  }, [posts]);
+
+  console.log(new Date().toISOString().slice(0, 10));
+
+  const onCreate = ({ title, content }) => {
+    const newPost = {
+      title: title,
+      content: content,
+      id: newId,
+      createAt: new Date().toISOString().slice(0, 10),
+    };
+    setPosts(prev => [...prev, newPost]);
+    return newPost.id;
+  };
+
+  const onUpdate = (_id, { title, content }) => {
+    setPosts(prev =>
+      prev.map(p =>
+        p.id === _id
+          ? {
+              ...p,
+              title: title,
+              content: content,
+            }
+          : p,
+      ),
+    );
+  };
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout loaded={loaded} />}>
+          <Route index element={<Home posts={posts} />} />
+          <Route path="posts" element={<Posts posts={posts} />} />
+          <Route path="post/:id" element={<PostDetail posts={posts} onDelete={onDelete} />} />
+          <Route path="post/edit/:id" element={<PostEdit posts={posts} onUpdate={onUpdate} />} />
+          <Route path="posts/new" element={<PostNew onCreate={onCreate} />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+export default function Home({ posts }) {
+  const latest = [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, 3);
+  console.log(latest);
+  return (
+    <section>
+      <h2>소개</h2>
+      <p>React Router로 목록/상세/작성/수정/삭제를 연습하는 미션입니다.</p>
+      <h3>최신 글</h3>
+      {posts.length === 0 ? (
+        <>
+          <p>글이 없습니다.</p>
+        </>
+      ) : (
+        <ul>
+          {latest.map(p => (
+            <li key={p.id}>
+              <Link to={`/post/${p.id}`}>{p.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
